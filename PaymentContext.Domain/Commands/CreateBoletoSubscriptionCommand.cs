2e7df916ac4e80e3359bc4fa91e3526d7a359369
Jhonathan Araujo <jhonathanaraujobr@hotmail.com>
2021-01-02 12:09:27 -0300

    Fail Fast Validantions

--- PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
+++ PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
@@ -1,9 +1,12 @@
 using System;
+using Flunt.Notifications;
+using Flunt.Validations;
 using PaymentContext.Domain.Enums;
+using PaymentContext.Shared.Commands;
 
 namespace PaymentContext.Domain.Commands
 {
-    public class CreateBoletoSubscriptionCommand
+    public class CreateBoletoSubscriptionCommand : Notifiable, ICommand
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -29,5 +32,15 @@ namespace PaymentContext.Domain.Commands
         public string State { get; set; }
         public string Country { get; set; }
         public string ZipCode { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(new Contract()
+                .Requires()
+                .HasMinLen(FirstName, 3, nameof(CreateBoletoSubscriptionCommand.FirstName), "Nome deve conter pelo menos 3 caracteres")
+                .HasMinLen(LastName, 3, nameof(CreateBoletoSubscriptionCommand.LastName), "Sobrenome deve conter pelo menos 3 caracteres")
+                .HasMaxLen(FirstName, 40, nameof(CreateBoletoSubscriptionCommand.FirstName), "Nome deve conter até 40 caracteres")
+            );
+        }
     }
 }
